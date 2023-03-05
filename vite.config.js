d52ff37da1ef4dcd491a7776e5f4e70ed46f741a
Cologler <skyoflw@gmail.com>
2023-03-06 02:24:47 +0800

    update to index.html
    seems like logseq wont fix pure js  plugin bug

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,3 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-    build: {
-        lib: {
-            entry: resolve(__dirname, 'src/index.ts'),
-            name: 'main',
-            fileName: 'index',
-            formats: ['umd']
-        }
-    },
-})
+export default defineConfig({})
